a34f6afdc98237cf12701a0ec6a31542b43cdce7
syntony666 <syntony666@outlook.com>
2020-03-23 17:55:25 +0800

    DELETE TRASH

--- Source/gamelib.cpp
+++ Source/gamelib.cpp
@@ -263,12 +263,6 @@ void CInteger::LoadBitmap()
 	//
 	// digit[i]為class varibale，所以必須避免重複LoadBitmap
 	//
-	if (!isBmpLoaded) {
-		int d[11]={IDB_0,IDB_1,IDB_2,IDB_3,IDB_4,IDB_5,IDB_6,IDB_7,IDB_8,IDB_9,IDB_MINUS};
-		for (int i=0; i < 11; i++)
-			digit[i].LoadBitmap(d[i],RGB(255,255,255));
-		isBmpLoaded = true;
-	}
 }
 
 void CInteger::SetInteger(int i)
--- Source/resource.h
+++ Source/resource.h
@@ -6,29 +6,16 @@
 #define IDP_SOCKETS_INIT_FAILED         104
 #define IDR_MAINFRAME                   128
 #define IDR_GAMETYPE                    129
-#define IDB_BALL                        130
 #define IDB_RACKET                      131
 #define IDB_BITMAP1                     132
-#define IDB_CORNER                      133
 #define IDB_BALL1                       134
 #define IDB_BALL2                       135
 #define IDB_BALL3                       136
 #define IDB_BALL4                       137
-#define IDB_0                           138
-#define IDB_1                           139
-#define IDB_2                           140
-#define IDB_3                           141
-#define IDB_4                           142
-#define IDB_5                           143
-#define IDB_6                           144
-#define IDB_7                           145
-#define IDB_8                           146
-#define IDB_9                           147
 #define IDB_MINUS                       148
 #define IDB_CENTER                      149
 #define IDB_CONTINUE                    152
 #define IDC_GAMECURSOR                  153
-#define IDB_HELP                        155
 #define IDB_BITMAP2                     156
 #define IDB_LOADING                     156
 #define IDB_BACKGROUND                  164
@@ -41,8 +28,9 @@
 #define IDB_FROG_GO_LEFT                173
 #define IDB_FROG_STAND_LEFT             176
 #define IDB_FROG_GO_LEFT2               178
-#define IDB_BITMAP4                     179
 #define IDB_FROG_GO_LEFT3               179
+#define IDB_BITMAP4                     182
+#define IDB_FROG_GO_LEFT1               182
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -55,7 +43,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        180
+#define _APS_NEXT_RESOURCE_VALUE        183
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
