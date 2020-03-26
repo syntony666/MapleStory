548dd347a95839f4cdb96c74d9222ef117b5fcef
syntony666 <syntony666@outlook.com>
2020-03-26 18:04:29 +0800

    add resource jump bmps

--- Source/resource.h
+++ Source/resource.h
@@ -6,6 +6,9 @@
 #define IDP_SOCKETS_INIT_FAILED         104
 #define IDR_MAINFRAME                   128
 #define IDR_GAMETYPE                    129
+#define IDB_RACKET                      131
+#define IDB_CONTINUE                    152
+#define IDC_GAMECURSOR                  153
 #define IDB_LOADING                     156
 #define IDB_BACKGROUND                  164
 #define IDB_LOGO                        166
@@ -16,9 +19,13 @@
 #define IDB_FROG_GO_LEFT                173
 #define IDB_FROG_STAND_LEFT             176
 #define IDB_FROG_GO_LEFT2               178
+#define IDB_FROG_GO_LEFT3               179
 #define IDB_FROG_GO_LEFT1               182
 #define IDB_FROG_GO_RIGHT1              183
 #define IDB_FROG_GO_RIGHT2              184
+#define IDB_FROG_JUMP_LEFT              185
+#define IDB_BITMAP1                     187
+#define IDB_FROG_JUMP_RIGHT             187
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -31,7 +38,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        186
+#define _APS_NEXT_RESOURCE_VALUE        188
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
