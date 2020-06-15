0cd5b1d60554f714258238e3a9b2e985296f7407
Nelson Chiang <nelson2wsx@yahoo.com.tw>
2020-06-15 16:54:48 +0800

    BMP Updated.

--- Source/resource.h
+++ Source/resource.h
@@ -273,6 +273,8 @@
 #define IDB_HEAL_08                     395
 #define IDB_BITMAP41                    396
 #define IDB_HEAL_09                     396
+#define IDB_BITMAP30                    397
+#define IDB_POISON_ROOT                 397
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -285,7 +287,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        397
+#define _APS_NEXT_RESOURCE_VALUE        398
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
