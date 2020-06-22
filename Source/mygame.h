8e14af5fdcff96813351d6db6943a128c72da65e
Nelson Chiang <nelson2wsx@yahoo.com.tw>
2020-06-22 17:12:27 +0800

    mygame.h finished.

--- Source/mygame.h
+++ Source/mygame.h
@@ -57,23 +57,23 @@ namespace game_framework
 	{
 	public:
 		CGameStateInit(CGame *g);
-		void OnInit();		 // 遊戲的初值及圖形設定
-		void OnBeginState(); // 設定每次重玩所需的變數
-		void OnKeyDown(UINT, UINT, UINT);
-		void OnLButtonDown(UINT nFlags, CPoint point); // 處理滑鼠的動作
+		void OnInit();									// 遊戲的初值及圖形設定
+		void OnBeginState();							// 設定每次重玩所需的變數
+		void OnKeyDown(UINT, UINT, UINT);				// 處理按下按鍵的反應
+		void OnLButtonDown(UINT nFlags, CPoint point);	// 處理滑鼠的動作
 	protected:
-		void OnShow(); // 顯示這個狀態的遊戲畫面
+		void OnShow();									// 顯示這個狀態的遊戲畫面
 	private:
-		CMovingBitmap logo; // csie的logo
-		CMovingBitmap info;
-		CMovingBitmap start1;
-		CMovingBitmap start2;
-		CMovingBitmap exit1;
-		CMovingBitmap exit2;
-		CMovingBitmap info1;
-		CMovingBitmap info2;
-		int menu = 1;	  //判別值 1=上 2=下
-		int ifInfoOn = 0; //說明選單 0=關 1=開
+		CMovingBitmap logo;		// 封面圖
+		CMovingBitmap info;		// 說明頁面
+		CMovingBitmap start1;	// 開始按鈕
+		CMovingBitmap start2;	// 開始按鈕(選擇中)
+		CMovingBitmap exit1;	// 離開按鈕
+		CMovingBitmap exit2;	// 離開按鈕(選擇中)
+		CMovingBitmap info1;	// 說明按鈕
+		CMovingBitmap info2;	// 說明按鈕(選擇中)
+		int menu = 1;			// 選單判定用
+		int ifInfoOn = 0;		// 說明選單 0=關 1=開
 	};
 
 	/////////////////////////////////////////////////////////////////////////////
@@ -86,38 +86,37 @@ namespace game_framework
 	public:
 		CGameStateRun(CGame *g);
 		~CGameStateRun();
-		void OnBeginState(); // 設定每次重玩所需的變數
-		void OnInit();		 // 遊戲的初值及圖形設定
+		void OnBeginState();							// 設定每次重玩所需的變數
+		void OnInit();									// 遊戲的初值及圖形設定
 		void OnKeyDown(UINT, UINT, UINT);
 		void OnKeyUp(UINT, UINT, UINT);
-		void OnLButtonDown(UINT nFlags, CPoint point); // 處理滑鼠的動作
-		void OnLButtonUp(UINT nFlags, CPoint point);   // 處理滑鼠的動作
-		void OnMouseMove(UINT nFlags, CPoint point);   // 處理滑鼠的動作
-		void OnRButtonDown(UINT nFlags, CPoint point); // 處理滑鼠的動作
-		void OnRButtonUp(UINT nFlags, CPoint point);   // 處理滑鼠的動作
+		void OnLButtonDown(UINT nFlags, CPoint point);	// 處理滑鼠的動作
+		void OnLButtonUp(UINT nFlags, CPoint point);	// 處理滑鼠的動作
+		void OnMouseMove(UINT nFlags, CPoint point);	// 處理滑鼠的動作
+		void OnRButtonDown(UINT nFlags, CPoint point);	// 處理滑鼠的動作
+		void OnRButtonUp(UINT nFlags, CPoint point);	// 處理滑鼠的動作
 	protected:
 		void OnMove(); // 移動遊戲元素
 		void OnShow(); // 顯示這個狀態的遊戲畫面
 	private:
 		int stage, stage_count;
-		CMovingBitmap slashCD[9], healCD[9];
-		CMovingBitmap background; // 背景圖
-		CMovingBitmap PoisonRoot;
-		CAnimation Boss_laser_delay, Boss_laser;
+		CMovingBitmap slashCD[9], healCD[9];				// 技能 CD 圖
+		CMovingBitmap background;							// 背景圖
+		CMovingBitmap PoisonRoot;							// 定身圖片
+		CAnimation Boss_laser_delay, Boss_laser;			// 這三行都是 BOSS 的技能動畫
 		CAnimation Boss_knockback_delay, Boss_knockback;
 		CAnimation Boss_lighting_delay1, Boss_lighting_delay2, Boss_lighting1, Boss_lighting2;
-		Character *hero;  // 角色
-		vector<Map> maps; // 地圖
+		Character *hero;									// 角色
+		vector<Map> maps;									// 地圖
 		Map *map;
-		vector<Character *> monster1, monster2, monster3; // 怪物
+		vector<Character *> monster1, monster2, monster3;	// 怪物
 		vector<Character *> *monster;
-		Boss boss;
-		Position hero_pos;
-		int temp = 300;
-		inline int monster_num(vector<Character *> monster);
-		void heroMonsterInteraction(Character &hero, vector<Character *> &monster, Map &map);
-		void heroBossInteraction(Character &hero, Boss &mboss, Map &map);
-		void checkStage();
+		Boss boss;											// BOSS
+		Position hero_pos;									// 角色座標位置
+		inline int monster_num(vector<Character *> monster);	// 怪物剩餘數量判定
+		void heroMonsterInteraction(Character &hero, vector<Character *> &monster, Map &map);	// 角色與怪物互動判定
+		void heroBossInteraction(Character &hero, Boss &mboss, Map &map);						// 角色與 BOSS 互動判定
+		void checkStage();									// 關卡檢測用
 	};
 
 	/////////////////////////////////////////////////////////////////////////////
@@ -136,14 +135,14 @@ namespace game_framework
 	protected:
 		void OnMove(); // 移動遊戲元素
 		void OnShow(); // 顯示這個狀態的遊戲畫面
-		void number_OnShow(int nx, int pos_nx, int pos_ny);
+		void number_OnShow(int nx, int pos_nx, int pos_ny); // 顯示數字 Bitmap 用
 
 	private:
 		int counter = 210; // 倒數之計數器
-		int clearTime = 0;
-		CAnimation Gameover;
-		CMovingBitmap clear_info;
-		CAnimation num[10];
+		int clearTime = 0; // 紀錄過關時間用
+		CAnimation Gameover; // 通關失敗動畫
+		CMovingBitmap clear_info; // 過關背景圖
+		CAnimation num[10]; // 顯示數字 Bitmap
 	};
 
 }
